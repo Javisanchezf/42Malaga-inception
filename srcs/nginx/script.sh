ad45d69f4db81c36167f1530bc206c14236fcd2c
Javisanchezf <javiersa@student.42malaga.com>
2024-01-30 14:48:21 +0100

    [2024-01-30 14:48:08] - Little changes by root

--- srcs/nginx/script.sh
+++ srcs/nginx/script.sh
@@ -10,10 +10,10 @@ events {
 http {
     server {
         listen 443 ssl http2;
-        server_name javiersa.42.fr;
+        server_name $DOMAIN_NAME;
 
-        ssl_certificate /etc/nginx/certs/javiersa.42.fr.crt;
-        ssl_certificate_key /etc/nginx/certs/javiersa.42.fr.key;
+        ssl_certificate /etc/nginx/certs/$DOMAIN_NAME.crt;
+        ssl_certificate_key /etc/nginx/certs/$DOMAIN_NAME.key;
 
         ssl_protocols TLSv1.2 TLSv1.3;
         ssl_ciphers 'TLS_AES_128_GCM_SHA256:TLS_AES_256_GCM_SHA384:TLS_CHACHA20_POLY1305_SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-RSA-AES256-GCM-SHA384';
