b1944484a1926c04432e92f0a7cda98d9435fc4d
Javisanchezf <javiersa@student.42malaga.com>
2024-02-05 11:34:20 +0100

    Daemon for wp instead of infinite bucle -to resolve wp admin

--- srcs/wordpress/run.sh
+++ srcs/wordpress/run.sh
@@ -10,31 +10,37 @@ if [ ! -d "/domains/$DOMAIN_NAME" ]; then
     wp config set DB_USER $DB_USER
     wp config set DB_PASSWORD $DB_PASS
     wp config set DB_HOST $DB_HOST
-
-    ##############################################
-    # WP_ALLOW_REPAIR
     wp config set DB_CHARSET utf8
     wp config set DB_COLLATE ''
+
+    ##############################################
+    # wp config set WP_ALLOW_REPAIR true
     wp config set AUTOMATIC_UPDATER_DISABLED true
     wp config set WP_LIMIT_LOGIN_ATTEMPTS true
     wp config set WP_DEBUG true
     wp config set WP_DEBUG_LOG true
     wp config set WP_DEBUG_DISPLAY false
-    # wp config set WP_CACHE true
     wp config set WP_AUTO_UPDATE_CORE minor
     wp config set WP_POST_REVISIONS false
     wp config set WP_MEMORY_LIMIT 96M
     wp config set WP_MAX_MEMORY_LIMIT 256M
     wp config set WP_CRON_LOCK_TIMEOUT 60
+
+
     wp core install --url=$DOMAIN_NAME --title=$DOMAIN_NAME --admin_user=$ADMIN_USER --admin_password=$ADMIN_PASS --admin_email=$ADMIN_EMAIL
-    wp theme install astra --activate --allow-root
-    wp plugin install wp-fastest-cache --activate --allow-root
+    # wp config set FORCE_SSL_ADMIN true
+    # wp config set WP_REDIS_HOST redis
+    # wp define WP_REDIS_PORT 6379
+    wp user create example example@example.com --role=author --user_pass=forty-two --allow-root
+    # wp theme install astra --activate --allow-root
+    # wp plugin install wp-fastest-cache --activate --allow-root
     wp plugin update --all --allow-root
+
+    wp rewrite structure '/%postname%/' --hard
     #Execute the commands regardless of the installed version of php-fpm, this being the latest available
     sed -i 's/^listen = .*/listen = 0.0.0.0:9000/' /etc/php*/php-fpm*.d/www.conf
     mv /usr/sbin/php-fpm* /usr/sbin/php-fpm
     mv /usr/bin/php-fpm* /usr/bin/php-fpm
-    php-fpm -R
 fi
 
-while true; do sleep 1000; done
+php-fpm -FR
