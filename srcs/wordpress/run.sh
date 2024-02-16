f0702c710bc9b0a9e51c6c89429689ee8b12ba09
Javisanchezf <javiersa@student.42malaga.com>
2024-02-16 13:19:36 +0100

    [2024-02-16 13:19:36] - Little changes by root

--- srcs/wordpress/run.sh
+++ srcs/wordpress/run.sh
@@ -32,19 +32,18 @@ wp config create \
   --dbuser="$DB_USER" \
   --dbpass="$DB_PASS" \
   --dbhost="$DB_HOST" \
-  --dbcharset=utf8mb4 \
-  --dbcollate='utf8mb4_unicode_ci' \
+  --dbcharset=utf8 \
+  --dbcollate='' \
   --extra-php <<PHP
-define( 'AUTOMATIC_UPDATER_DISABLED', true );
-define( 'WP_LIMIT_LOGIN_ATTEMPTS', true );
-define( 'WP_DEBUG', true );
-define( 'WP_DEBUG_LOG', true );
-define( 'WP_DEBUG_DISPLAY', false );
-define( 'WP_AUTO_UPDATE_CORE', 'minor' );
-define( 'WP_POST_REVISIONS', false );
-define( 'WP_MEMORY_LIMIT', '96M' );
-define( 'WP_MAX_MEMORY_LIMIT', '256M' );
-define( 'WP_CRON_LOCK_TIMEOUT', 60 );
+define('AUTOMATIC_UPDATER_DISABLED', true);
+define('WP_LIMIT_LOGIN_ATTEMPTS', true);
+define('WP_DEBUG', true);
+define('WP_DEBUG_LOG', true);
+define('WP_DEBUG_DISPLAY', false);
+define('WP_AUTO_UPDATE_CORE', 'minor');
+define('WP_POST_REVISIONS', false);
+define('EMPTY_TRASH_DAYS', 7);
+define('FS_METHOD', 'direct');
 PHP
 
 wp config shuffle-salts --allow-root
@@ -69,8 +68,8 @@ wp plugin update --all --allow-root
 echo -e "${GREEN}Rewriting WordPress permalinks...${NC}"
 wp rewrite structure '/%postname%/' --hard
 
-echo -e "${GREEN}Configuring PHP-FPM...${NC}"
-sed -i 's/^listen = .*/listen = 0.0.0.0:9000/' /etc/php*/php-fpm*.d/www.conf
+# echo -e "${GREEN}Configuring PHP-FPM...${NC}"
+# sed -i 's/^listen = .*/listen = 0.0.0.0:9000/' /etc/php*/php-fpm*.d/www.conf
 
 echo -e "${GREEN}Starting PHP-FPM...${NC}"
 php-fpm -FR
